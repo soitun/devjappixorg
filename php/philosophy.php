4a79afeb6459196591be741a0af317863383a225
Julien Barrier <julien@julienbarrier.fr>
2012-01-29 14:34:04 +0100

    link modifications

--- php/philosophy.php
+++ php/philosophy.php
@@ -11,13 +11,13 @@
 
 <article class="left" style="font-size: 1.2em; line-height:1.1em;">
 <h3>An idea of a better web</h3>
-<p>Jappix is an XMPP web client. We think that make a better web is one of the most important thing. Therefore, we try to respect the web standards and try to use as much as possible HTML5 and CSS3</p>
-<p>The web is more and more social. One of Jappix’ goals are to foster communication. We don’t want a centralized web, that’s why you can manage your own Jappix node. We don’t want your data to be scattered all over the internet, that’s why we use XMPP, a universal protocol that helps you to be closer!</p>
+<p>Jappix is an XMPP web client. We think that making a better web is one of the most important thing. Therefore, we try to respect the web standards and try to use HTML5 and CSS3 as much as possible</p>
+<p>The web is more and more social. One of Jappix’ goals is to foster communication. We don’t want a centralized web, that’s why you can manage your own Jappix node. We don’t want your data to be scattered all over the internet, that’s why we use XMPP, a universal protocol that helps you be closer!</p>
 </article>
 <h2>Non profit project managment</h2>
 <article  class="width" style="font-size: 1.2em; line-height: 1.1em;">
 <p style="padding-left: 50px;">Moreover, Jappix is managed by <a href="http://post-pro.fr">PostPro</a> french non-profit organization. This guarantees that Jappix developers won’t earn any money of their work. We’ve made Jappix because we believe in a better web. Of course, you can donate to our project. But it will be <a href="http://www.post-pro.fr/soutien-en">just used</a> for our servers, to participate to events (as LSM or other conferences)…</p>
 <p style="padding-left: 50px;">Don't forget, Jappix is yours. Have an idea? Feel free to implement it then <a href="https://github.com/jappix/jappix">send us a patch</a> and we'll incorporate in the official release!</p>
-<p style="padding-left: 50px;">Want to help but you don't have idea? No problem, you're in the right place! You can go to <a href="http://developer.jappix.org/0.9.1">our roadmap</a> and implement new features, or you can go to <a href="https://github.com/jappix/jappix/issues">our bugtracker</a> to fix bugs!</p>
+<p style="padding-left: 50px;">Want to help but you don't have any idea? No problem, you're in the right place! You can go to <a href="http://developer.jappix.org/0.9.1">our roadmap</a> and implement new features, or you can go to <a href="https://github.com/jappix/jappix/issues">our bugtracker</a> to fix bugs!</p>
 <p>We hope you’ll enjoy Jappix!</p>
 </article>
--- php/welcome.php
+++ php/welcome.php
@@ -3,7 +3,7 @@
 <!--[if lt IE 9]><div style="font-size: x-big"><p>You browse the web with Internet Explorer</p><p>This web browser don’t support technologies used on this website<p>
 <p style="height: 20px;"></p>
 <p style="font-weight: bold">We recommand you to use <a href="http://www.mozilla-europe.org/" >Mozilla Firefox</a> instead.</p></div><![endif]-->
-<p>Jappix is 100% free software. It means everybody who doesn’t like something or who want new functionnalities can modify the program and make it better. Building a better social web is one of our goals. Help us now !</p>
+<p>Jappix is 100% free software. It means everybody who doesn’t like something or who want new functionnalities can modify the program and make it better. Building a better social Web is one of our goals. Help us now!</p>
 <?php $version = parseXML(readCacheFile('version', 'http://jappix.org/xml/version.xml')); ?>
 <p style="clear:both; height:20px;"></p>
 	<a href="/<?php echo($version['next']) ?>" class=get>
@@ -12,29 +12,29 @@
 		<span class=second>Check what we planned for Jappix next release</span>
 	</a>
 <p style="clear:both; height:20px;"></p>
-<p style="font-size: x-big;">Maybe you wanna learn something more about</p>
+<p style="font-size: x-big;">Maybe you wanna learn something more about:</p>
 <ul><li><a href="/philosophy">Our philosophy</a></li>
 <li><a href="calendar">The release calendar</a></li>
 </ul>
 <p style="font-size: x-big;">You can also grab the code by copying the line below:<br/>
-<code>git clone git://github.com/Jappix/jappix.git</code></p>
+<code>git clone git://github.com/jappix/jappix.git</code></p>
 </article>
 <p style="clear:both; height:60px;"></p>
 <section class=description>
 
 	<details class=droite><a href=http://codingteam.net/project/jappix/i18n>
 		<h2>Help Translate</h2>
-		<p>If you have language knowledges, help us to make Jappix better for everyone !</p>
+		<p>If you have language knowledges, help us to make Jappix better for everyone!</p>
 	</a></details>
 
 	<details class=milieu><a href=/communication>
 		<h2>Spread It</h2>
-		<p>As Jappix is made by a non-profit organization, we need your help to tell the world how Jappix is good !</p>
+		<p>As Jappix is made by a non-profit organization, we need your help to tell the world how Jappix is good!</p>
 	</a></details>
 
 	<details class=gauche><a href=/run>
 		<h2>Run a Node</h2>
-		<p>The software is made to be decentralized. If you run a pod, you’ll help us to make a better web !</p>
+		<p>The software is made to be decentralized. If you run a pod, you’ll help us to make a better web!</p>
 	</a></details>
 
 </section>
